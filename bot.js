6a54c693c472ba7befafce1f490103edf3c16469
MarioDev74 <38961849+MarioDev74@users.noreply.github.com>
2018-05-03 20:10:57 -0500

    Update bot.js
    Prueba de escribir archivo.

--- bot.js
+++ bot.js
@@ -4,19 +4,21 @@ var auth = require('./auth.json');
 var AWS = require('aws-sdk');
 
 const fs = require("fs");
-var gamertags// = JSON.parse(data);
+var gamertags // = JSON.parse(data);
+var s3 = new AWS.S3();
 
 var params = {
-    Bucket: 'gtbot', Key: 'gamertags.json' };
-new AWS.S3().getObject(params, function (err, json_data) {
+    Bucket: 'gtbot', Key: 'gamertags.json'
+};
+s3.getObject(params, function (err, json_data) {
     if (!err) {
         gamertags = JSON.parse(new Buffer(json_data.Body).toString("utf8"));
     } else {
         logger.info(err.message);
         gamertags = JSON.parse('{"mariocms": {"gamertag": "MarioFake"}}');
     }
-   });
- 
+});
+
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(logger.transports.Console, {
@@ -25,8 +27,8 @@ logger.add(logger.transports.Console, {
 logger.level = 'debug';
 // Initialize Discord Bot
 var bot = new Discord.Client({
-   token: process.env.BOT_TOKEN,
-   autorun: true
+    token: process.env.BOT_TOKEN,
+    autorun: true
 });
 bot.on('ready', function (evt) {
     logger.info('Connected');
@@ -41,10 +43,10 @@ bot.on('message', function (user, userID, channelID, message, evt) {
         var cmd = args[0];
         var par = args[1];
         var par2 = args[2];
-       
+
         args = args.splice(1);
-         
-        switch(cmd) {                
+
+        switch (cmd) {
             case 'gt':
                 if (par != null) {
                     if (gamertags.hasOwnProperty(par)) {
@@ -63,14 +65,19 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                     bot.sendMessage({
                         to: channelID,
                         message: 'El comando utilizado requiere un nombre de usuario para buscar su GT.'
-                    });                
+                    });
                 };
                 break;
             case 'agregargt':
                 if (par != null && par2 != null) {
                     if (gamertags.hasOwnProperty(par)) {
                         gamertags[par].gamertag = par2;
-                        fs.writeFile("./gamertags.json", JSON.stringify(gamertags, null, 4));
+                        //fs.writeFile("./gamertags.json", JSON.stringify(gamertags, null, 4));
+                        params = {
+                            Bucket: 'gtbot', Key: 'gamertags.json', Body: JSON.stringify(gamertags, null, 4)
+                        };
+                        logger.info(params)
+                        s3.putObject(params, function (resp) {logger.info('GT agregado para ' + par)});
                         bot.sendMessage({
                             to: channelID,
                             message: 'El GT ' + par2 + ' ha sido modificado para el usuario ' + par
@@ -125,13 +132,13 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                 });
                 break;
             // Just add any case commands if you want to..
-                
-                /*   Mensaje de En Mantenimiento
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Por el momento, GTBot se encuentra en mantenimiento. Disculpa las molestias. Pronto volvera con toda la funcionalidad!'
-                });
-                break;*/
-         }
-     }
+
+            /*   Mensaje de En Mantenimiento
+            bot.sendMessage({
+                to: channelID,
+                message: 'Por el momento, GTBot se encuentra en mantenimiento. Disculpa las molestias. Pronto volvera con toda la funcionalidad!'
+            });
+            break;*/
+        }
+    }
 });
